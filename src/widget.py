9d76c9603c0dcd524fe368210826af805e39104d
Your Name <you@example.com>
2025-11-20 22:28:28 +0400

    widget fixes

--- src/widget.py
+++ src/widget.py
@@ -5,11 +5,18 @@ from masks import get_mask_account
 
 
 def mask_account_card (coded_accounts: str) -> str:
+    """ Принимает один аргумент, возращает строку с замаскированным номером"""
+
     decoded = coded_accounts.split()
     only_numbers = decoded[-1]
     only_name = decoded[:-1]
-    if only_name == "Счет":
+
+    if "cчет" in coded_accounts.lower():
         masked_numbers = get_mask_account(only_numbers)
     else:
         masked_numbers = get_mask_card_number(only_numbers)
     return f"{only_name} {masked_numbers}"
+
+
+def get_date (dates: str) -> str:
+    return dates[8:10] + "." + dates[5:7] + "." + dates[:4]
