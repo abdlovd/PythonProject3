7842635b662c9102a6587374e42a7deced36575a
Your Name <you@example.com>
2025-12-01 13:52:40 +0400

    additional changes

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,9 @@
-def filter_by_state (data, state: str ) -> str :
+def filter_by_state (data, state: str ) -> list [str]:
     """ Принимает список словарей, возвращает новый список словарей у которых ключ соответствует указанному значению"""
 
+    sorted_list = []
+
     for item in data:
-        sorted_list = []
         if item [state] == 'EXECUTED':
             sorted_list.append(item)
     return sorted_list
