cfe22bf85d3b62ee35f6ec95bb61f5c0b655b656
Your Name <you@example.com>
2025-12-01 13:48:14 +0400

    changes in func

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,11 @@
-def filter_by_state (state: list[str] ) -> list[str]:
+def filter_by_state (data, state: str ) -> str :
     """ Принимает список словарей, возвращает новый список словарей у которых ключ соответствует указанному значению"""
 
-    sorted_list = []
-    second_list = []
-
-    if state == 'EXECUTED':
-        sorted_list.append(state)
-    else:
-        second_list.append(state)
-    return sorted_list, second_list
+    for item in data:
+        sorted_list = []
+        if item [state] == 'EXECUTED':
+            sorted_list.append(item)
+    return sorted_list
 
 
 def sort_by_date (date: list[str]) -> list:
